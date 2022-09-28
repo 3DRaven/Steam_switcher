84a9f4115b72a4dd1c568a3804ba98230f22ec21
Renat Eskenin <3draven@gmail.com>
2022-09-28 14:55:04 +0200

    Set reaction to only closing main window

--- src/steam_close.lua
+++ src/steam_close.lua
@@ -2,7 +2,7 @@ local default_main_screen="DP-4"
 local sleep_time_sec=5
 local default_sink_for_work = "bluez_output.6C_47_60_EC_7B_EB.a2dp-sink"
 
-if string.match(get_class_instance_name(), "[sS]team") then
+if string.match(get_class_instance_name(), "Steam") and string.match(get_window_name(), "Steam") then
     debug_print("Found Steam window closing")
     os.execute(string.format('(pactl set-default-sink "%s") &', default_sink_for_work))
     os.execute(string.format("(xrandr --output %s --primary) &",default_main_screen))
